fe00c086ec04e205d8557a66907c0064693ec0eb
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-11 10:13:15 +0000

    Add metric calculation in validation

--- src/graphnet/models/standard_model.py
+++ src/graphnet/models/standard_model.py
@@ -1,6 +1,6 @@
 """Standard model class(es)."""
 
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import torch
 from torch import Tensor
@@ -8,6 +8,7 @@ from torch.nn import ModuleList
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torch_geometric.data import Data
+from torchmetrics.aggregation import CatMetric
 
 from graphnet.models.coarsening import Coarsening
 from graphnet.utilities.config import save_model_config
@@ -51,6 +52,48 @@ def state_norm(module: torch.nn.Module, norm_type: Union[float, int, str], group
     return norms
 
 
+class MeanCatMetric(CatMetric):
+    """Concatenate a stream of values, mean on compute.
+
+    As input to ``forward`` and ``update`` the metric accepts the following input
+
+    - ``value`` (:class:`~float` or :class:`~torch.Tensor`): a single float or an tensor of float values with
+      arbitary shape ``(...,)``.
+
+    As output of `forward` and `compute` the metric returns the following output
+
+    - ``agg`` (:class:`~torch.Tensor`): scalar float tensor with concatenated values over all input received
+
+    Args:
+        nan_strategy: options:
+            - ``'error'``: if any `nan` values are encounted will give a RuntimeError
+            - ``'warn'``: if any `nan` values are encounted will give a warning and continue
+            - ``'ignore'``: all `nan` values are silently removed
+            - a float: if a float is provided will impude any `nan` values with this value
+
+        kwargs: Additional keyword arguments, see :ref:`Metric kwargs` for more info.
+
+    Raises:
+        ValueError:
+            If ``nan_strategy`` is not one of ``error``, ``warn``, ``ignore`` or a float
+
+    Example:
+        >>> import torch
+        >>> from torchmetrics import CatMetric
+        >>> metric = CatMetric()
+        >>> metric.update(1)
+        >>> metric.update(torch.tensor([2, 3]))
+        >>> metric.compute()
+        tensor(2.)
+    """
+    def compute(self) -> Tensor:
+        """Compute the aggregated value."""
+        value = super().compute()
+        if isinstance(value, list) and not value:
+            return torch.tensor(0.0)
+        return value.mean()
+
+
 class StandardModel(Model):
     """Main class for standard models in graphnet.
 
@@ -72,6 +115,7 @@ class StandardModel(Model):
         scheduler_class: Optional[type] = None,
         scheduler_kwargs: Optional[Dict] = None,
         scheduler_config: Optional[Dict] = None,
+        metric_fn: Optional[Callable] = None,
         **kwargs: Any,
     ) -> None:
         """Construct `StandardModel`."""
@@ -99,6 +143,8 @@ class StandardModel(Model):
         self._scheduler_class = scheduler_class
         self._scheduler_kwargs = scheduler_kwargs or dict()
         self._scheduler_config = scheduler_config or dict()
+        self._metric_fn = metric_fn
+        self._metric_agg = MeanCatMetric()
         self.log_norm_verbose = kwargs.get("log_norm_verbose", False)
 
     def configure_optimizers(self) -> Dict[str, Any]:
@@ -140,11 +186,11 @@ class StandardModel(Model):
         """
         preds = self(batch)
         loss, losses = self.compute_loss(preds, batch)
-        return loss, losses
+        return loss, losses, preds
 
     def training_step(self, train_batch: Data, batch_idx: int) -> Tensor:
         """Perform training step."""
-        loss, losses = self.shared_step(train_batch, batch_idx)
+        loss, losses, _ = self.shared_step(train_batch, batch_idx)
         self.log(
             "train_loss",
             loss,
@@ -166,7 +212,7 @@ class StandardModel(Model):
 
     def validation_step(self, val_batch: Data, batch_idx: int) -> Tensor:
         """Perform validation step."""
-        loss, losses = self.shared_step(val_batch, batch_idx)
+        loss, losses, preds = self.shared_step(val_batch, batch_idx)
         self.log(
             "val_loss",
             loss,
@@ -184,7 +230,29 @@ class StandardModel(Model):
             on_step=False,
             sync_dist=True,
         )
+        if self._metric_fn is not None:
+            # Metric is element-wise, sum is provided to
+            # .log, reduce_fx calculates the mean over 
+            # validation dataloader
+            elementwise_metric_values = self._metric_fn(
+                [pred.detach() for pred in preds], 
+                val_batch
+            )
+            self._metric_agg(elementwise_metric_values)
+            self.log(
+                "val_metric",
+                self._metric_agg,
+                batch_size=None,
+                prog_bar=True,
+                on_epoch=True,
+                on_step=False,
+                sync_dist=True,
+            )
         return loss
+
+    def on_validation_epoch_end(self) -> None:
+        """Perform validation epoch end."""
+        self._metric_agg.reset()
 
     def compute_loss(
         self, preds: Tensor, data: Data, verbose: bool = False
